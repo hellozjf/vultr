09687437e274121f1cc4ecf24d8d566de39c0a2b
Jingfeng Zhou <908686171@qq.com>
2018-07-24 17:05:26 +0800

    update vultr_system.sh

--- vultr_system.sh
+++ vultr_system.sh
@@ -8,7 +8,17 @@ echo "vultr.hellozjf.com" >/etc/hostname
 systemctl stop firewalld
 systemctl disable firewalld
 
-# 3. 配置交换参数，0尽可能使用内存，100尽可能使用交换分区，网上说10比较好
+# 3. 配置交换分区
+fallocate -l 1G /swapfile
+chmod 600 /swapfile
+mkswap /swapfile
+swapon /swapfile
+if [ ! -f "/etc/fstab_backup" ]; then
+    cp /etc/fstab /etc/fstab_backup
+    echo "/swapfile   swap    swap    sw  0   0" >>/etc/fstab	
+fi
+
+# 4. 配置交换参数，0尽可能使用内存，100尽可能使用交换分区，网上说10比较好
 line=`cat -n /etc/sysctl.conf | grep "vm.swappiness" | awk '{print $1}'`
 if [ ! $line ]; then
     echo "vm.swappiness = 10" >>/etc/sysctl.conf
@@ -16,11 +26,14 @@ else
     sed -i $line'c vm.swappiness = 10' /etc/sysctl.conf
 fi
 
-# 4. 安装traceroute
+# 5. 安装traceroute
 yum install -y traceroute.x86_64
 
-# 5. 安装bbr
+# 6. 安装bbr
 wget https://raw.githubusercontent.com/hellozjf/vultr/master/bbr.sh && chmod +x bbr.sh && bash bbr.sh
 
-# 6. 修改root的密码
+# 7. 修改root的密码
 echo root:Zjf@12340z3j@3f8 | chpasswd
+
+# 8. 安装net-tools
+yum install -y net-tools
