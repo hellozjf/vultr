8e08682ff508ade8e7387370d5d9ddea71d78c80
Jingfeng Zhou <908686171@qq.com>
2018-07-25 00:02:10 +0800

    update

--- vultr_docker.sh
+++ vultr_docker.sh
@@ -2,11 +2,11 @@
 # 为vultr安装docker
 
 # 1. 安装docker
-yum install -y docker
+#yum install -y docker
 
 # 2. 启动docker
-systemctl start docker
-systemctl enable docker
+#systemctl start docker
+#systemctl enable docker
 
 # 3. 安装shadowsocks
 docker run -d -p 10101:10101 --name ss --restart unless-stopped mritd/shadowsocks -s "-s 0.0.0.0 -p 10101 -m aes-256-cfb -k Zjf@1234 --fast-open"
