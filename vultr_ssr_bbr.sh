412d25c2dd83d641958043a1144cf14ba56bdc23
Jingfeng Zhou <908686171@qq.com>
2018-07-24 20:21:47 +0800

    update vultr_ssr_bbr.sh

--- vultr_ssr_bbr.sh
+++ vultr_ssr_bbr.sh
@@ -2,7 +2,7 @@
 # 安装ssr和bbr
 
 wget https://raw.githubusercontent.com/hellozjf/doubi/master/ssr.sh && chmod +x ssr.sh
-wget https://raw.githubusercontent.com/hellozjf/doubi/master/bbr.sh && chmod +x bbr.sh
+wget https://raw.githubusercontent.com/hellozjf/across/master/bbr.sh && chmod +x bbr.sh
 yum install -y expect
 wget https://raw.githubusercontent.com/hellozjf/vultr/master/vultr_ssr_expect.sh && chmod +x vultr_ssr_expect.sh && ./vultr_ssr_expect.sh
 wget https://raw.githubusercontent.com/hellozjf/vultr/master/vultr_bbr_expect.sh && chmod +x vultr_bbr_expect.sh && ./vultr_bbr_expect.sh
